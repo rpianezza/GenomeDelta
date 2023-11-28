bf53a6be475cd83e51c2a1fd3635c3d74cafbf1d
Riccardo Pianezza <rpianezza97@gmail.com>
2023-11-28 14:14:12 +0100

    Modified launcher

--- macOS/launcher.py
+++ macOS/launcher.py
@@ -1,10 +1,15 @@
 import os
 import subprocess
 import sys
+import argparse
+
+# Parse command-line arguments
+parser = argparse.ArgumentParser(description="GenomeDelta launcher")
+parser.add_argument("--main", help="Path to main file")
+args = parser.parse_args()
 
 if __name__ == "__main__":
-    #os.environ["PATH"] = "/usr/local/Caskroom/miniconda/base/envs/GenomeDelta/bin:" + os.environ["PATH"]
-    script_path = os.path.abspath("GenomeDelta/macOS/main.sh")
+    script_path = args.main
 
     # Give execute permission and run the script with arguments
     subprocess.call(["chmod", "+x", script_path])
--- macOS/setup.sh
+++ macOS/setup.sh
@@ -15,13 +15,16 @@ conda activate GenomeDelta
 # Get the path to the conda environment's bin directory
 conda_bin_path=$(conda info --envs | grep "^GenomeDelta " | awk '{print $NF}')/bin
 
+# Get the path to "main.sh"
+main_script_path=$(pwd)/main.sh
+
 # Create the executable file
 python -m pip install pyinstaller
-pyinstaller launcher.py --onefile --name GenomeDelta
+pyinstaller --onefile --name GenomeDelta launcher.py -- --main "$main_script_path"
 
 # Copy the executable file to the conda environment's bin directory
 mv dist/GenomeDelta "$conda_bin_path"
 
 # Print information about the setup
-echo "Setup complete."
-echo "To launch the software, type GenomeDelta and add the requested arguments (--fq/--bam, --fa, --of, --t)"
+echo "Setup complete. Type "conda activate GenomeDelta" to activate the environment."
+echo "To launch the software, type GenomeDelta in the environment and add the requested arguments (--fq/--bam, --fa, --of, --t)"
