d112fdca990169046d1304da39a388eed0621818
Riccardo Pianezza <rpianezza97@gmail.com>
2023-11-28 12:12:57 +0100

    Update launcher

--- macOS/launcher.py
+++ macOS/launcher.py
@@ -4,7 +4,7 @@ import sys
 
 if __name__ == "__main__":
     #os.environ["PATH"] = "/usr/local/Caskroom/miniconda/base/envs/GenomeDelta/bin:" + os.environ["PATH"]
-    script_path = os.path.abspath("main.sh")
+    script_path = os.path.abspath("GenomeDelta/main.sh")
 
     # Give execute permission and run the script with arguments
     subprocess.call(["chmod", "+x", script_path])
